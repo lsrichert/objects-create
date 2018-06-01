2a6e8d4a4ea41df3ae1864426089a9631215b361
Lauren Richert <lsrichert@gmail.com>
2018-06-01 10:35:22 -0500

    created objects

---
+++ main.js
@@ -0,0 +1,120 @@
+const OmoreCollege = Object.create({}, {
+    business: {
+        enumerable: true,
+        writable: false,
+        value: "OmoreCollege"
+    },
+    employmentStart: {
+        enumerable: true,
+        writable: false,
+        value: "07-11-16"
+    },
+    employmentEnd: {
+        enumerable: true,
+        writable: false,
+        value: "05-31-18"
+
+    },
+    goodThings: {
+        enumerable: true,
+        writable: false,
+        value: "Great co-workers"
+    },
+    badThings: {
+        enumerable: true,
+        writable: false,
+        value: "Long commute"
+    }
+})
+console.log(OmoreCollege)
+
+const DepaulUniversity = Object.create({}, {
+    business: {
+        enumerable: true,
+        writable: false,
+        value: "DePaul University"
+    },
+    employmentStart: {
+        enumerable: true,
+        writable: false,
+        value: "12-30-10"
+    },
+    employmentEnd: {
+        enumerable: true,
+        writable: false,
+        value: "03-15-15"
+    },
+    goodThings: {
+        enumerable: true,
+        writable: false,
+        value: "Great environment"
+    },
+    badThings: {
+        enumerable: true,
+        writable: false,
+        value: "Low pay"
+    }
+
+})
+console.log(DepaulUniversity)
+
+const ibmc = Object.create({}, {
+    business: {
+        enumerable: true,
+        writable: false,
+        value: "IBMC College"
+    },
+    employmentStart: {
+        enumerable: true,
+        writable: false,
+        value: "09-15-15"
+    },
+    employmentEnd: {
+        enumerable: true,
+        writable: false,
+        value: "07-01-16"
+    },
+    goodThings: {
+        enumerable: true,
+        writable: false,
+        value: "Great people"
+    },
+    badThings: {
+        enumerable: true,
+        writable: false,
+        value: "Bad corporate leadership"
+    }
+
+}) 
+console.log(ibmc)
+
+const oldNavy = Object.create({}, {
+    business: {
+        enumerable: true,
+        writable: false,
+        value: "Old Navy"
+    },
+    employmentStart: {
+        enumerable: true,
+        writable: false,
+        value: "05-01-00"
+    },
+    employmentEnd: {
+        enumerable: true,
+        writable: false,
+        value: "05-25-09"
+    },
+    goodThings: {
+        enumerable: true,
+        writable: false,
+        value: "Fun place to be"
+    },
+    badThings: {
+        enumerable: true,
+        writable: false,
+        value: "Terrible schedule"
+    }
+
+})
+console.log(oldNavy)
+
